84b178d76bc3b63fc43810fbfe45aada4872e34e
mqprichard <mqprichard@gmail>
2013-07-12 11:14:46 -0700

    Added message as parameter to asyncSend() and added JavaDoc

--- src/main/java/com/cloudbees/gasp/services/GCMMessageService.java
+++ src/main/java/com/cloudbees/gasp/services/GCMMessageService.java
@@ -10,6 +10,10 @@ import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
+/**
+ *  Generic interface to GCM (Google Cloud Messaging) based on Android SDK GCM sample app
+ *  Supports asynchronous multicasting of messages to 5 devices at a time
+ */
 public class GCMMessageService {
     private static final int MULTICAST_SIZE = 1000;
     private static final String key = "AIzaSyD8RPFcX_YY3-M21yGGaww2_NBPLHsjU5o";
@@ -18,45 +22,53 @@ public class GCMMessageService {
     private static final Logger LOGGER = LoggerFactory.getLogger(GCMMessageService.class.getName());
     private static final Executor threadPool = Executors.newFixedThreadPool(5);
 
+    /**
+     * Sends a message via GCM. Calls asyncSend() to multicast messages asynchronously
+     * @param message      The GCM message to send - create using Message.Builder()
+     * @throws IOException
+     */
     public void sendMessage(Message message) throws IOException {
         List<String> devices = Datastore.getDevices();
 
         if (devices.isEmpty()) {
             LOGGER.error("Message ignored as there is no device registered!");
         } else {
-            if (devices.size() == 1) {
-                String registrationId = devices.get(0);
-                // Message message = new Message.Builder().build();
-                Result result = sender.send(message, registrationId, 5);
-                LOGGER.info("Sent message to one device: " + result);
-            } else {
-                int total = devices.size();
-                List<String> partialDevices = new ArrayList<String>(total);
-                int counter = 0;
-                int tasks = 0;
-                for (String device : devices) {
-                    counter++;
-                    partialDevices.add(device);
-                    int partialSize = partialDevices.size();
-                    if (partialSize == MULTICAST_SIZE || counter == total) {
-                        asyncSend(partialDevices);
-                        partialDevices.clear();
-                        tasks++;
-                    }
+            int total = devices.size();
+            List<String> partialDevices = new ArrayList<String>(total);
+            int counter = 0;
+            int tasks = 0;
+            for (String device : devices) {
+                counter++;
+                partialDevices.add(device);
+                int partialSize = partialDevices.size();
+                if (partialSize == MULTICAST_SIZE || counter == total) {
+                    asyncSend(message, partialDevices);
+                    partialDevices.clear();
+                    tasks++;
                 }
-                LOGGER.info("Asynchronously sending " + tasks + " multicast messages to " +
-                        total + " devices");
             }
+            LOGGER.info("Asynchronously sending " + tasks + " multicast messages to " +
+                        total + " devices");
         }
     }
 
-    private void asyncSend(List<String> partialDevices) {
-        // make a copy
+    /**
+     * Handles the actual dispatch of messages to GCM service. Messages are sent asynchronously
+     * using threadpool.execute(). The Datastore is updated if an appplication has changed its
+     * regId or is no longer available on the device.
+     *
+     * @param msg            The GCM message to multicast
+     * @param partialDevices The array of devices to receive the message
+     */
+    private void asyncSend(Message msg, List<String> partialDevices) {
+
         final List<String> devices = new ArrayList<String>(partialDevices);
+        final Message message = msg;
+
         threadPool.execute(new Runnable() {
 
             public void run() {
-                Message message = new Message.Builder().build();
+                // Multicast message to 5 devices and collect the results
                 MulticastResult multicastResult;
                 try {
                     multicastResult = sender.send(message, devices, 5);
@@ -65,12 +77,13 @@ public class GCMMessageService {
                     return;
                 }
                 List<Result> results = multicastResult.getResults();
-                // analyze the results
+
                 for (int i = 0; i < devices.size(); i++) {
                     String regId = devices.get(i);
                     Result result = results.get(i);
                     String messageId = result.getMessageId();
                     if (messageId != null) {
+                        // Success: message sent to this device
                         LOGGER.info("Succesfully sent message to device: " + regId +
                                 "; messageId = " + messageId);
                         String canonicalRegId = result.getCanonicalRegistrationId();
